8febbbb4bb87eb3d28ad1975912ce03f2762b9d9
Loz <tuanchu24112004@gmail.com>
2024-07-12 22:02:32 +0700

    gradle and color

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,9 +36,22 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
-    implementation("com.airbnb.android:lottie:6.0.0")
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    //Loading Animation
+    implementation("com.airbnb.android:lottie:6.0.0")
+    //Room (for Database)
+    implementation("androidx.room:room-runtime:2.5.2")
+    annotationProcessor("androidx.room:room-compiler:2.5.2")
+    //RecyclerView
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    //Scalable size unit(support for different screen size)
+    implementation("com.intuit.sdp:sdp-android:1.0.6")
+    implementation("com.intuit.ssp:ssp-android:1.0.6")
+    //Material design
+    implementation("com.google.android.material:material:1.1.0")
+    //Rounded imageView
+    implementation("com.makeramen:roundedimageview:2.3.0")
 }
