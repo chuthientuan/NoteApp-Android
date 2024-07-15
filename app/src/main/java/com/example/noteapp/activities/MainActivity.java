33ac2ddb40170b9252acdfacdf2843f809dd9d18
Loz <tuanchu24112004@gmail.com>
2024-07-15 15:50:14 +0700

    Display Recycleview

--- app/src/main/java/com/example/noteapp/activities/MainActivity.java
+++ app/src/main/java/com/example/noteapp/activities/MainActivity.java
@@ -9,21 +9,30 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.example.noteapp.R;
+import com.example.noteapp.adapter.NoteAdapter;
 import com.example.noteapp.database.NotesDatabase;
 import com.example.noteapp.entities.Note;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_CREATE_NOTE = 1;
     private long pressBacktime;
     ImageView imgAddNote, imgAddNoteMain;
+
+    private RecyclerView recyclerViewNotes;
+    private List<Note> noteList;
+    private NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +50,18 @@ public class MainActivity extends AppCompatActivity {
                 new Intent(getApplicationContext(), CreateNoteActivity.class),
                 REQUEST_CREATE_NOTE)
         );
-        imgAddNoteMain.setOnClickListener(v -> startActivityForResult(new Intent(getApplicationContext(), CreateNoteActivity.class),
+        imgAddNoteMain.setOnClickListener(v -> startActivityForResult(
+                new Intent(getApplicationContext(), CreateNoteActivity.class),
                 REQUEST_CREATE_NOTE)
         );
+        recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
+        recyclerViewNotes.setLayoutManager(
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        );
+        noteList = new ArrayList<>();
+        noteAdapter = new NoteAdapter(noteList);
+        recyclerViewNotes.setAdapter(noteAdapter);
+
         getNote();
     }
 
@@ -58,7 +76,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
-                Log.d("MY_NOTES", notes.toString());
+                if (noteList.size() == 0) {
+                    noteList.addAll(notes);
+                    noteAdapter.notifyDataSetChanged();
+                }
+                else {
+                    noteList.add(0, notes.get(0));
+                    noteAdapter.notifyItemInserted(0);
+                }
             }
         }
         new GetNotesTask().execute();
@@ -74,5 +99,13 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show();
         }
         pressBacktime = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CREATE_NOTE && resultCode == RESULT_OK) {
+            getNote();
+        }
     }
 }
